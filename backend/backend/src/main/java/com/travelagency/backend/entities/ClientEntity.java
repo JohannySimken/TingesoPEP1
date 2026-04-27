556400f281a61e714dcc2a040517c821915f1f73
Johanny Simken <johanny.simken@usach.cl>
2026-04-26 20:22:16 -0400

    feat: Added Client controller, service, repository with the respective endpoint

---
+++ backend/backend/src/main/java/com/travelagency/backend/entities/ClientEntity.java
@@ -0,0 +1,60 @@
+package com.travelagency.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+
+
+@Entity
+@Table(name = "clients")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClientEntity {
+    public enum Role {CLIENT, ADMIN    }
+    public enum Status {ACTIVE, INACTIVE
+
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String phone;
+    private String nationality;
+    private String identityDocument;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Builder.Default
+    private Integer failedLoginAttempts = 0;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate (){
+        this.createdAt = LocalDateTime.now();
+        if (this.role == null) {this.role = Role.CLIENT;}
+        if (this.status == null) {this.status = Status.ACTIVE;}
+        if (this.failedLoginAttempts == null) {this.failedLoginAttempts = 0;}
+    }
+
+}
